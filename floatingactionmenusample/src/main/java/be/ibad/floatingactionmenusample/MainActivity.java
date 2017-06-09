966edc5ada6ded07f8f21c7fcee93ec805a79659
Nicolas Van Damme <vandamme.n@gmail.com>
2017-06-09 20:14:32 +0200

    Improve sample app

--- floatingactionmenusample/src/main/java/be/ibad/floatingactionmenusample/MainActivity.java
+++ floatingactionmenusample/src/main/java/be/ibad/floatingactionmenusample/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
 
 import be.ibad.FloatingActionMenu;
 import be.ibad.listener.OnMenuItemClickListener;
@@ -17,17 +16,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FloatingActionMenu actionMenu = (FloatingActionMenu) findViewById(R.id.action_menu);
+        final FloatingActionMenu actionMenu = (FloatingActionMenu) findViewById(R.id.action_menu);
         actionMenu.addOnMenuItemClickListener(new OnMenuItemClickListener() {
             @Override
             public void onMenuItemClick(FloatingActionMenu floatingActionMenu, int index, FloatingActionButton item) {
-                Snackbar.make(findViewById(R.id.coordinator), item.getContentDescription(), Snackbar.LENGTH_SHORT).show();
+                Snackbar.make(findViewById(R.id.coordinator), item.getContentDescription() + " " + index, Snackbar.LENGTH_SHORT).show();
+                switch (item.getId()) {
+                    case R.id.bad_item:
+                        actionMenu.setOverlayColorRes(R.color.transparentColorPrimary);
+                        actionMenu.setMenuButtonColorRes(R.color.colorPrimary);
+                        actionMenu.setMenuRippleColorRes(R.color.colorAccent);
+                        actionMenu.setMenuButtonRes(R.drawable.ic_mood_bad_white_24dp);
+                        actionMenu.setLabelTextColorRes(R.color.colorAccent);
+                        break;
+                    case R.id.happy_item:
+                        actionMenu.setMenuButtonColorRes(R.color.colorAccent);
+                        actionMenu.setOverlayColorRes(R.color.transparentColorAccent);
+                        actionMenu.setMenuRippleColorRes(R.color.colorPrimary);
+                        actionMenu.setMenuButtonRes(R.drawable.ic_mood_white_24dp);
+                        actionMenu.setLabelTextColorRes(R.color.colorPrimary);
+                        break;
+                }
             }
         });
         actionMenu.addOnMenuToggleListener(new OnMenuToggleListener() {
             @Override
             public void onMenuToggle(boolean opened) {
-                Toast.makeText(MainActivity.this, "Toggle " + opened, Toast.LENGTH_SHORT).show();
+
             }
         });
     }
